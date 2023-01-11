f8b5a013e264ea9678bbbe45e3e630874f68d22e
Diego Beltrán Maldonado <diego.beltranm.95@outlook.com>
2023-01-11 17:27:03 -0500

    first commit

---
+++ app/src/main/java/com/dbappgame/koinloginapp/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.dbappgame.koinloginapp.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
